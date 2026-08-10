345a96a546507ab26574b143cdc4db9b7c63f043
Yhprum <ryanmurphy@gatech.edu>
2026-08-10 17:03:02 -0400

    make the scroll bar easier to use

--- src/renderer/components/Layout.tsx
+++ src/renderer/components/Layout.tsx
@@ -8,7 +8,7 @@ export default function Layout() {
       <Sidebar />
       <div className="flex flex-col flex-1 min-w-0">
         <StatusBar />
-        <main className="flex-1 overflow-y-auto p-6">
+        <main className="scrollbar-edge flex-1 overflow-y-auto p-6">
           <Outlet />
         </main>
       </div>
